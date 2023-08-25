15292d31919ea8355b301f07d63f7bfa390afbfd
Pedro Coelho <pedro.coelho@moreharvest.com>
2023-08-25 22:38:09 +0800

    :sparkles: PWA setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,23 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/service-worker.js")
+        .then((registration) => {
+          registration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: process.env.NEXT_PUSH_APP_KEY,
+          });
+          console.log("scope is: ", registration.scope);
+        });
+    }
+  }, []);
+
   return (
     <>
       <Head>
